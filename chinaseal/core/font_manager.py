53adf2257efa80734676556929281a63e0754962
chentaoxing <chentaoxing@gmail.com>
2026-08-31 05:34:35 +0800

    chore: update chinaseal/core/font_manager.py

--- chinaseal/core/font_manager.py
+++ chinaseal/core/font_manager.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024-2026 chentaoxing <[email protected]>
+# Copyright (C) 2024-2026 chentaoxing <chentaoxing@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 r"""字体管理：系统字体文件枚举、自定义字体注册、缺字检测。
 
