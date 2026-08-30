40d23f151f42ee88a0a418243660188ec16c65d4
chentaoxing <chentaoxing@gmail.com>
2026-08-31 05:34:30 +0800

    chore: update chinaseal/core/__init__.py

--- chinaseal/core/__init__.py
+++ chinaseal/core/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024-2026 chentaoxing <[email protected]>
+# Copyright (C) 2024-2026 chentaoxing <chentaoxing@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 from . import font_manager, layout, model, outlines, paths, project_io  # noqa: F401
