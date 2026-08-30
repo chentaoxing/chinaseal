d81449d04ccb83d6cbd69a06ac4eccf6d4b91bd0
chentaoxing <chentaoxing@gmail.com>
2026-08-31 05:34:27 +0800

    chore: update chinaseal/__init__.py

--- chinaseal/__init__.py
+++ chinaseal/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024-2026 chentaoxing <[email protected]>
+# Copyright (C) 2024-2026 chentaoxing <chentaoxing@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 """ChinaSeal · 中国篆刻印稿工坊"""
-__version__ = "0.2.0"
+__version__ = "0.3.3"
