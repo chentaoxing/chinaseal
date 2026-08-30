e328477d71006add2ab8d05d01a54adfc9663f6a
chentaoxing <chentaoxing@gmail.com>
2026-08-31 05:34:55 +0800

    chore: update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024-2026 chentaoxing <[email protected]>
+# Copyright (C) 2024-2026 chentaoxing <chentaoxing@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 """PyInstaller 打包入口（包外脚本，必须用绝对导入）。"""
 from chinaseal.__main__ import main
