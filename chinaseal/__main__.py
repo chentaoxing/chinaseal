1269d828acffc2301afcf4327cc7cac44ef0286b
chentaoxing <chentaoxing@gmail.com>
2026-08-31 05:34:28 +0800

    chore: update chinaseal/__main__.py

--- chinaseal/__main__.py
+++ chinaseal/__main__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2024-2026 chentaoxing <[email protected]>
+# Copyright (C) 2024-2026 chentaoxing <chentaoxing@gmail.com>
 # SPDX-License-Identifier: GPL-3.0-only
 """ChinaSeal 入口。
 
